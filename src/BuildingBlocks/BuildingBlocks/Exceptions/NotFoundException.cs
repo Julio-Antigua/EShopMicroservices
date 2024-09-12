627ccaf8d4b9f0aafe138825bc0904a96c61c00e
Julio Saul <juliosaul12@gmail.com>
2024-09-12 12:15:03 -0400

    Global exceptions and custom exceptions were implemented

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace BuildingBlocks.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string mesaage) : base(mesaage)
+        {
+                
+        }
+
+        public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+                
+        }
+    }
+}
