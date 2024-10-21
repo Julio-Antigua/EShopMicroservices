8dbaddf0faf323b81d428b4ec63e1c83813bc504
Julio Saul <juliosaul12@gmail.com>
2024-10-21 11:06:10 -0400

    creacion de los modelos de shoppingcart

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCarItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    //Required for Mapping
+    public ShoppingCart() { }
+}
